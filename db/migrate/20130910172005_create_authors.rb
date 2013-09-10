9bbed0c2e7680cede08b4e100ab06ea9fea2ad28
Louise Au Yeung <louise@eabx.org>
2013-09-10 14:20:32 -0400

    created models without migrating

---
+++ db/migrate/20130910172005_create_authors.rb
@@ -0,0 +1,12 @@
+class CreateAuthors < ActiveRecord::Migration
+  def change
+    create_table :authors do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :phone
+      t.text :address
+
+      t.timestamps
+    end
+  end
+end
